1cf49fbb75059e04122049ed362f23e5fdf87ff6
Munana Clinton <munanaclinton@gmail.com>
2023-04-24 20:04:16 +0200

    Add changes to index.html and main.js

--- main.js
+++ main.js
@@ -1,24 +1,52 @@
-const nameOfBookField = document.querySelector('#name-of-book');
-const authorOfBookField = document.querySelector('#author-of-book');
-const addBook = document.querySelector('.add-book-button');
-const form = document.querySelector('form')
-const bookName = document.querySelector('.name-of-book'); 
+const addBookBtn = document.querySelector('.add-book-button'); 
+const bookList = document.querySelector('#book-list');
+
+let books = [];
 
 
-
-form.addEventListener('submit', (e)=>{
-    const bookShelf=[
-        {
-            nameOfbook : nameOfBookField.value,
-            authorOfBook : authorOfBookField.value, 
-        }
-    ];
-    if(bookShelf[0].nameOfbook !== '' && bookShelf[0].authorOfBook !== ''){
-         e.preventDefault();
-         console.log(bookShelf);
-         bookName.innerHTML=bookShelf[0].nameOfbook;
-
+const displayBooks = () =>{
+    const book = localStorage.getItem('book');
+    if(book === null){
+        books=[];
+    } else {
+      books=JSON.parse(book)
     }
-    nameOfBookField.value="";
-    authorOfBookField.value="";
+   let listOfbook="";
+   books.forEach((item, index)=>{
+    listOfbook+=`<div>
+    <p class="name-of-book">${item.title}</p>
+    <p class="author-of-book">${item.author}</p>
+    <button class="remove-book-button" onclick="removeBook(${index})">Remove</button>
+    <hr>
+    </div>`
+   })
+   bookList.innerHTML=listOfbook;
+}
+
+function AddBooks (title , author){
+   const book = localStorage.getItem('book');
+   if(book === null){
+    books = [];
+   } else {
+     books=JSON.parse(book);
+   }
+   books.push(
+    {title, 
+    author,
+    })
+   localStorage.setItem('book', JSON.stringify(books))
+   displayBooks();
+}
+
+addBookBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    const title = document.getElementById('name-of-book').value;
+    const author = document.getElementById('author-of-book').value;
+
+    AddBooks(title, author);
 })
+
+
+
+
+displayBooks();

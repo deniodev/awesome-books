60d1dc2690e2d7de8dabc1f1667f7cb1496542cd
Munana Clinton <munanaclinton@gmail.com>
2023-04-25 16:12:54 +0200

    Add change to html, css and js

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
-//Declare the Book class
+// Declare the Book class
 class Book {
   constructor(title, author) {
-      this.title = title;
-      this.author = author;
+    this.title = title;
+    this.author = author;
   }
 }
 
-//Declare the Display Class
+// Declare the Display Class
 class Display {
-   static displayBooks() {
-      const books = Store.getBooks();
-      books.forEach((book) => {
-        Display.addBookToList(book);
-      });
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach((book) => {
+      Display.addBookToList(book);
+    });
   }
 
-   static addBookToList(book) {
-    const list = document.querySelector('#book-list'); 
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
     const newBook = document.createElement('div');
     list.appendChild(newBook);
-    newBook.innerHTML = `
-    <p>${book.title}</p>
-    <p>${book.author}</p>
-    <button class="remove-book-button">Remove</button>
-    <hr>`;      
-    
+    newBook.className = 'my-book';
+    newBook.innerHTML = `<div class="book-name-and-author">
+    <p>${book.title}</p> <span>by</span>
+    <p>${book.author}</p></div>
+    <button class="remove-book-button">Remove</button></div>`;
   }
 
   static removeBook(book) {
-    if(book.classList.contains('remove-book-button')) {
+    if (book.classList.contains('remove-book-button')) {
       book.parentElement.remove();
     }
   }
@@ -37,53 +36,51 @@ class Display {
     document.getElementById('name-of-book').value = '';
     document.getElementById('author-of-book').value = '';
   }
-
 }
 
 // Preserve data in a browser using LocalStorage
 class Store {
-static getBooks() {
-  let books;
-  if (localStorage.getItem('books') === null) {
-    books = [];
-  } else {
-    books = JSON.parse(localStorage.getItem('books'));
-  }
-  return books;
-}
-
-static addBook(book) {
-  const books = Store.getBooks();
-  //
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-static deleteBook(title) {
-  const books = Store.getBooks();
-
-  books.forEach((book, index) => {
-    if (book.title === title) {
-        books.splice(index, 1);
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
     }
-  });
+    return books;
+  }
 
-  localStorage.setItem('books', JSON.stringify(books)); 
-}
-}
+  static addBook(book) {
+    const books = Store.getBooks();
+    //
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 
+  static deleteBook(title) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.title === title) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
 
 document.addEventListener('DOMContentLoaded', Display.displayBooks);
 
-//Add Book Event Listener
+// Add Book Event Listener
 document.querySelector('#book-form').addEventListener('submit', (e) => {
   e.preventDefault();
   const nameOfBookField = document.querySelector('#name-of-book').value;
   const authorOfBookField = document.querySelector('#author-of-book').value;
 
-  if(nameOfBookField === '' || authorOfBookField === '') {
-    Display;
-  } else {
+  if (nameOfBookField === '' || authorOfBookField === '') {
+    return;
+  }
   const book = new Book(nameOfBookField, authorOfBookField);
 
   Display.addBookToList(book);
@@ -91,25 +88,12 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   Store.addBook(book);
 
   Display.clearInputs();
-  }
 });
 
 const bookList = document.getElementById('book-list');
-//Remove Book event Listener
+// Remove Book event Listener
 bookList.addEventListener('click', (e) => {
-Display.removeBook(e.target);
+  Display.removeBook(e.target);
 
-
-Store.deleteBook(e.target.previousElementSibling.previousElementSibling.textContent); 
-
+  Store.deleteBook(e.target.previousElementSibling.firstElementChild.textContent);
 });
-
-
-
-
-
-
-
-
-
-
